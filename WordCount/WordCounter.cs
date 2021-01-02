624b1b92da8fe5df009e17a61cee9a589b7912bd
heather dougherty <heatherdougherty1234@me.com>
2021-01-01 23:29:34 -0500

    fixed formatting

--- WordCount/WordCounter.cs
+++ WordCount/WordCounter.cs
@@ -3,32 +3,32 @@ using System.Linq;
 
 namespace WordCount
 {
-	public class WordCounter
-	{
-		/// <summary>
-		/// Returns the number of the words in the phrase argument.  Assume that the phrase is a single line and
-		/// that words are only delimited by spaces.  A word must contain at least 1 non-space character
-		/// but does not necessarily need to be valid English. 
-		/// </summary>
-		public int CountWords(string phrase)
-		{
-			if (phrase== null)
+  public class WordCounter
+  {
+    /// <summary>
+    /// Returns the number of the words in the phrase argument.  Assume that the phrase is a single line and
+    /// that words are only delimited by spaces.  A word must contain at least 1 non-space character
+    /// but does not necessarily need to be valid English. 
+    /// </summary>
+    public int CountWords(string phrase)
+    {
+      if (phrase == null)
       {
-				return 0;
+        return 0;
       }
 
-			int c = 0;
-			var segments = phrase.Split(' ');
+      int c = 0;
+      var segments = phrase.Split(' ');
 
-			foreach( string segment in segments)
+      foreach (string segment in segments)
       {
-				if(segment != "")
+        if (segment != "")
         {
-					c++;
+          c++;
         }
       }
 
-			return c;
-		}
-	}
+      return c;
+    }
+  }
 }
